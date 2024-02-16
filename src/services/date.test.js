9c3c07ca986bf1697cd314fed87222c779515a81
Rafael Camargo <contato@rafaelcamargo.com>
2024-02-16 16:09:46 -0300

    chore: improve date service test

--- src/services/date.test.js
+++ src/services/date.test.js
@@ -2,8 +2,8 @@ const dateService = require('./date');
 
 describe('Date Service', () => {
   it('should get now date', () => {
-    const now = new Date().getTime();
-    const serviceNow = dateService.getNow().getTime();
+    const [now] = new Date().toISOString().split('.');
+    const [serviceNow] = dateService.getNow().toISOString().split('.');
     expect(serviceNow).toEqual(now);
   });
 });
